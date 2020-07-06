5f59d89d5e658f9728bf5aa6e46dce55b8c18db4
Mikhail Kalinin <werf121@rambler.ru>
2020-07-06 23:46:05 +0400

    TestFile

---
+++ test.py
@@ -0,0 +1,16 @@
+import os
+import requests
+
+# print(os.listdir(os.path.join('C:\учеба\основы python\9', 'files'))[0])
+# print(os.listdir(r'C:\учеба\основы python\9\files')[0])
+# path = os.path.join('C:\учеба\нетология\основы Py\9', 'files')
+path = r'C:/учеба/нетология/основы Py/9/files'
+for symbol
+print(path)
+
+# headers = {'Authorization': 'OAuth AgAAAAA86I9wAADLW4TMFaX010u7pssNYZgHB8w'}
+# params = {'path': path}
+# # print(params)
+# resp = requests.get('https://cloud-api.yandex.net:443/v1/disk/resources', params = params, headers=headers)
+# print(resp)
+# print(resp.text)
