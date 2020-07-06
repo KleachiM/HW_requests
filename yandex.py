7ff201e920bb764f498ada0b82b96a57b74f65ac
Mikhail Kalinin <werf121@rambler.ru>
2020-07-06 23:43:00 +0400

    Yandex v.1

--- yandex.py
+++ yandex.py
@@ -1,20 +1,26 @@
 import requests
 import os
 
-headers = {'Authorization': 'OAuth AgAAAAA86I9wAADLW4TMFaX010u7pssNYZgHB8w'}
-
 class YaUploader:
     def __init__(self, file_path: str):
         self.file_path = file_path
 
+    def create(self):
+        """Метод создает папку на Яндекс.Диске"""
+        headers = {'Authorization': f'OAuth {token}'}
+        params = {'path': self.file_path}
+        resp = requests.put('https://cloud-api.yandex.net:443/v1/disk/resources', params = params, headers = headers)
+
     def upload(self):
         """Метод загруджает файлы по списку file_list на яндекс диск"""
-        resp = requests.put ( 'https://cloud-api.yandex.net:443/v1/disk/resources?path=%5CC%5Cnew%5C',
-                              headers = headers )
+        file_list = os.listdir(self.file_path)
+        for file in file_list:
+
         return 'Вернуть ответ об успешной загрузке'
 
 
 if __name__ == '__main__':
-    path = os.path.join ( 'C:\учеба\нетология\основы Py\9', 'files' )
+    path = r'C:\учеба\основы python\9\files'
+    token = 'AgAAAAA86I9wAADLW4TMFaX010u7pssNYZgHB8w'
     uploader = YaUploader(path)
     result = uploader.upload()
