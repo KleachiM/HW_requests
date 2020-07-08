5e99eba45b32095262da657d199f5eb7e4fdb8cd
Mikhail Kalinin <werf121@rambler.ru>
2020-07-08 20:43:22 +0300

    yandex 08/07/2020 v1

--- yandex.py
+++ yandex.py
@@ -8,22 +8,23 @@ class YaUploader:
     def __init__(self, file_path: str, token: str):
         self.params = {'path': file_path}
         self.headers = {'Authorization': f'OAuth {token}'}
-        if self.find_path() == 404:
-            self.create()
+        # if self.find_path() == 404:
+        #     self.create()
 
-    def find_path(self):
-        """Метод проверяет, существует ли папка на Яндекс.Диске"""
-        resp = requests.get (self.url, params=self.params, headers = self.headers)
-        return resp.status_code
-
-    def create(self):
-        """Метод создает папку на Яндекс.Диске"""
-        resp = requests.put(self.url, params=self.params, headers = self.headers)
+    # def find_path(self):
+    #     """Метод проверяет, существует ли папка на Яндекс.Диске"""
+    #     resp = requests.get (self.url, params=self.params, headers = self.headers)
+    #     return resp.status_code
+    #
+    # def create(self):
+    #     """Метод создает папку на Яндекс.Диске"""
+    #     resp = requests.put(self.url, params=self.params, headers = self.headers)
 
     def upload(self):
         """Метод загруджает файлы по списку file_list на яндекс диск"""
         resp = requests.get(self.upl_url, params = self.params, headers = self.headers)
-        put_url = resp.json().get( 'href' )
+        print(resp.text)
+        # put_url = resp.json().get( 'href' )
         # print(put_url)
 
 
