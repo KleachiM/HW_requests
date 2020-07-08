f471d20ee38d7043811d5196462319d04fb8b6a4
Mikhail Kalinin <werf121@rambler.ru>
2020-07-08 07:43:30 +0300

    08.07.2020 7:43

--- test.py
+++ test.py
@@ -4,13 +4,12 @@ import requests
 # print(os.listdir(os.path.join('C:\учеба\основы python\9', 'files'))[0])
 # print(os.listdir(r'C:\учеба\основы python\9\files')[0])
 # path = os.path.join('C:\учеба\нетология\основы Py\9', 'files')
-path = r'C:/учеба/нетология/основы Py/9/files'
-for symbol
-print(path)
+path = r'C\new'
 
-# headers = {'Authorization': 'OAuth AgAAAAA86I9wAADLW4TMFaX010u7pssNYZgHB8w'}
-# params = {'path': path}
-# # print(params)
-# resp = requests.get('https://cloud-api.yandex.net:443/v1/disk/resources', params = params, headers=headers)
-# print(resp)
-# print(resp.text)
+
+headers = {'Authorization': 'OAuth AgAAAAA86I9wAADLW4TMFaX010u7pssNYZgHB8w'}
+params = {'path': path}
+# print(params)
+resp = requests.get('https://cloud-api.yandex.net:443/v1/disk/resources', params = params, headers=headers)
+print(resp.status_code)
+print(resp.text)
--- yandex.py
+++ yandex.py
@@ -2,25 +2,33 @@ import requests
 import os
 
 class YaUploader:
-    def __init__(self, file_path: str):
-        self.file_path = file_path
+    url = 'https://cloud-api.yandex.net:443/v1/disk/resources'
+    upl_url = 'https://cloud-api.yandex.net:443/v1/disk/resources/upload'
+
+    def __init__(self, file_path: str, token: str):
+        self.params = {'path': file_path}
+        self.headers = {'Authorization': f'OAuth {token}'}
+        if self.find_path() == 404:
+            self.create()
+
+    def find_path(self):
+        """Метод проверяет, существует ли папка на Яндекс.Диске"""
+        resp = requests.get (self.url, params=self.params, headers = self.headers)
+        return resp.status_code
 
     def create(self):
         """Метод создает папку на Яндекс.Диске"""
-        headers = {'Authorization': f'OAuth {token}'}
-        params = {'path': self.file_path}
-        resp = requests.put('https://cloud-api.yandex.net:443/v1/disk/resources', params = params, headers = headers)
+        resp = requests.put(self.url, params=self.params, headers = self.headers)
 
     def upload(self):
         """Метод загруджает файлы по списку file_list на яндекс диск"""
-        file_list = os.listdir(self.file_path)
-        for file in file_list:
-
-        return 'Вернуть ответ об успешной загрузке'
+        resp = requests.get(self.upl_url, params = self.params, headers = self.headers)
+        put_url = resp.json().get( 'href' )
+        # print(put_url)
 
 
 if __name__ == '__main__':
-    path = r'C:\учеба\основы python\9\files'
+    path = r'C\new'
     token = 'AgAAAAA86I9wAADLW4TMFaX010u7pssNYZgHB8w'
-    uploader = YaUploader(path)
-    result = uploader.upload()
+    uploader = YaUploader(path, token)
+    uploader.upload()
