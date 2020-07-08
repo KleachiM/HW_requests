0ea790ba1c7acf3f8200ba53e28a2129e725bb3b
Mikhail Kalinin <werf121@rambler.ru>
2020-07-08 23:55:11 +0400

    Осталось доделать requests.put

--- test.py
+++ test.py
@@ -1,15 +1,5 @@
 import os
 import requests
 
-# print(os.listdir(os.path.join('C:\учеба\основы python\9', 'files'))[0])
-# print(os.listdir(r'C:\учеба\основы python\9\files')[0])
-# path = os.path.join('C:\учеба\нетология\основы Py\9', 'files')
-path = r'C\new'
-
-
-headers = {'Authorization': 'OAuth AgAAAAA86I9wAADLW4TMFaX010u7pssNYZgHB8w'}
-params = {'path': path}
-# print(params)
-resp = requests.get('https://cloud-api.yandex.net:443/v1/disk/resources', params = params, headers=headers)
-print(resp.status_code)
-print(resp.text)
+with open(r'C:\учеба\основы python\9\files\file1.txt') as f:
+    print(f.readline())
--- yandex.py
+++ yandex.py
@@ -5,9 +5,11 @@ class YaUploader:
     url = 'https://cloud-api.yandex.net:443/v1/disk/resources'
     upl_url = 'https://cloud-api.yandex.net:443/v1/disk/resources/upload'
 
-    def __init__(self, file_path: str, token: str):
-        self.params = {'path': file_path}
+    def __init__(self, file_path: str, file_name: str, token: str):
+        self.params = {'path': f'/{file_name}'}
         self.headers = {'Authorization': f'OAuth {token}'}
+        self.file_path = file_path
+        self.file_name = file_name
         # if self.find_path() == 404:
         #     self.create()
 
@@ -22,14 +24,17 @@ class YaUploader:
 
     def upload(self):
         """Метод загруджает файлы по списку file_list на яндекс диск"""
-        resp = requests.get(self.upl_url, params = self.params, headers = self.headers)
-        print(resp.text)
-        # put_url = resp.json().get( 'href' )
-        # print(put_url)
+        resp = requests.get(self.upl_url, params=self.params, headers=self.headers)
+        put_url = resp.json().get('href')
+
+        files = {'file': open(self.file_path + self.file_name, 'rb')}
+        resp = requests.put(self.upl_url, files=files)
+        return resp.text
 
 
 if __name__ == '__main__':
-    path = r'C\new'
+    path = 'C:/учеба/основы python/9/files/'
     token = 'AgAAAAA86I9wAADLW4TMFaX010u7pssNYZgHB8w'
-    uploader = YaUploader(path, token)
-    uploader.upload()
+    file_name = 'file1.txt'
+    uploader = YaUploader(path, file_name, token)
+    print(uploader.upload())
